969cb24469aecd64d6572003592b7108891a5e62
Marc Maninang <Marc.Maninang@WINSFORD.COM>
2018-06-14 14:06:05 -0700

    Updated Header Content

--- popup.php
+++ popup.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- *  Plugin Name:    Modal popup Plugin
+ *  Plugin Name:    Modal Popup Plugin
  *  Plugin URI:     https://harloff.com
  *  Description:    This is a plugin for controlling a Modal Popup. I created this plugin as a way to request for feedback from our users.
  *  Version:        1.0
@@ -9,8 +9,7 @@
  *  License:        GPL2
  *  License URI:    https://www.gnu.org/licenses/gpl-2.0.html
  */
-
-    defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 
     
