fbd4a9e1394b2a7d8cf2032e1ac15c1ac2c14aa7
Marc Maninang <Marc.Maninang@WINSFORD.COM>
2018-06-14 12:08:01 -0700

    First commit. Added standard plugin header info and block direct access to plugin files

---
+++ popup.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ *  Plugin Name:    Modal popup Plugin
+ *  Plugin URI:     https://harloff.com
+ *  Description:    This is a plugin for controlling a Modal Popup. I created this plugin as a way to request for feedback from our users.
+ *  Version:        1.0
+ *  Author:         Marc Reyes-Maninang
+ *  License:        GPL2
+ *  License URI:    https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+    defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+
+    
+?>
