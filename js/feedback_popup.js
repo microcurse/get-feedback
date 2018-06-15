fc680ac338321c2b8f18cde9bf237c2b5adbe7e2
Marc Maninang <Marc.Maninang@WINSFORD.COM>
2018-06-15 16:23:38 -0700

    Added fancbox html for the pop up

--- js/feedback_popup.js
+++ js/feedback_popup.js
@@ -2,24 +2,13 @@ function openFancyBox() {
     setTimeout( function() {$('#feedback-box').trigger('click'); }, 20000);
 }
 
-function popUpHtml() {
-    alert('Pop Up HTML is now working.');
-    // document.write('<div class="pop-container">');
-    // document.write('<div class="pop-content">');
-    // document.write('This is a test of the content');
-    // document.write('</div>');
-    // document.write('</div>');
-}
-
 jQuery(document).ready(function($) {
-    alert('It worked');
-
-    // var visited = $.cookie('visited');
-    // if (visited == 'yes') {
-    //     return false;
-    // } else {
-    //     openFancyBox();
-    // }
-    // $.cookie('visited', 'yes', {expires: 7});
-    // $('#feedback-box').fancybox();
+    var visited = $.cookie('visited');
+    if (visited == 'yes') {
+        return false;
+    } else {
+        openFancyBox();
+    }
+    $.cookie('visited', 'yes', {expires: 7});
+    $('#feedback-box').fancybox();
 });
