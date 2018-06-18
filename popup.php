85b0bf72835e40ce885ceb78a5bb564cf8717f18
Marc Maninang <Marc.Maninang@WINSFORD.COM>
2018-06-18 14:13:09 -0700

    Updated Plugin name and description

--- popup.php
+++ popup.php
@@ -1,8 +1,8 @@
 <?php
 /**
- *  Plugin Name:    Modal Popup Plugin
+ *  Plugin Name:    Get Feedback (Fancybox Popup) 
  *  Plugin URI:     https://harloff.com
- *  Description:    This is a plugin for controlling a Modal Popup. I created this plugin as a way to request for feedback from our users.
+ *  Description:    This plugin is for displaying a modal pop up to get user feedback on the site. It uses jQuery and Fancybox to display the pop up and jscookie to track if a user has already seen this popup.
  *  Version:        1.0
  *  Author:         Marc Reyes-Maninang
  *  License:        GPL2
@@ -13,7 +13,7 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 function feedback_popup() {
     wp_enqueue_script( 'fancybox', 'https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.js', array( 'jquery' ), '3.3.5', true);
     wp_enqueue_script( 'feedback_popup', plugins_url( '/popup/js/feedback_popup.js'), array( 'fancybox', 'jquery' ), '1', true);
-    wp_enqueue_script( 'js_cookies', 'https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js', true );
+    wp_enqueue_script( 'js_cookie', 'https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js', true );
     wp_enqueue_style( 'fancybox', 'https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.css', '3.3.5');
 }
 
