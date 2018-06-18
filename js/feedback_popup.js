22d445ad91ca43089d301ac7968f2747674ef7c6
Marc Maninang <Marc.Maninang@WINSFORD.COM>
2018-06-18 11:37:44 -0700

    Added cookies checking. Added html to fancybox iframe loading.

--- js/feedback_popup.js
+++ js/feedback_popup.js
@@ -1,14 +1,20 @@
 // This is for delaying and displaying the pop up after a few seconds.
 function openFancyBox() {
     setTimeout( function() {
-        jQuery('#feedback').trigger('click'); 
+        // Load pop up html
+        jQuery.fancybox.open({
+            src     : 'https://harloff.staging.wpengine.com/wp-content/plugins/popup/fancybox.html',
+            type    : 'iframe',
+            opts    : {
+                afterShow : function( instance, current ) {
+                    console.info( 'done!' );
+                }
+            }
+        }); 
     }, 2000);
 }
 
 jQuery(document).ready(function($) {
-    // Load pop up html
-    $('#page').load('https://harloff.staging.wpengine.com/wp-content/plugins/popup/fancybox.html');
-
     // This checks whether or not the user has seen the pop up
     var visited = $.cookie('visited');
     if (visited == 'yes') {
--- popup.php
+++ popup.php
@@ -13,6 +13,7 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 function feedback_popup() {
     wp_enqueue_script( 'fancybox', 'https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.js', array( 'jquery' ), '3.3.5', true);
     wp_enqueue_script( 'feedback_popup', plugins_url( '/popup/js/feedback_popup.js'), array( 'fancybox', 'jquery' ), '1', true);
+    wp_enqueue_script( 'js_cookies', 'https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js', true );
     wp_enqueue_style( 'fancybox', 'https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.css', '3.3.5');
 }
 
