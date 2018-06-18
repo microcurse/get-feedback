be65920131a5aa928a09a4dd1a3e0a54e081fa26
Marc Maninang <Marc.Maninang@WINSFORD.COM>
2018-06-18 11:17:58 -0700

    Added comments

--- js/feedback_popup.js
+++ js/feedback_popup.js
@@ -1,14 +1,15 @@
-// This is for delaying and displaying the pop up after a few minutes.
+// This is for delaying and displaying the pop up after a few seconds.
 function openFancyBox() {
     setTimeout( function() {
         jQuery('#feedback').trigger('click'); 
-    }, 1000);
+    }, 2000);
 }
 
 jQuery(document).ready(function($) {
-
+    // Load pop up html
     $('#page').load('https://harloff.staging.wpengine.com/wp-content/plugins/popup/fancybox.html');
 
+    // This checks whether or not the user has seen the pop up
     var visited = $.cookie('visited');
     if (visited == 'yes') {
         return false;
